cce5ed411c6d84a97aa79f9fbda718765d04cfe4
takez0_o <takez0_o@protonmail.com>
2023-09-18 09:16:27 +0300

    remove cakebot

--- src/lib/common/StatusBar.svelte
+++ src/lib/common/StatusBar.svelte
@@ -1,6 +1,6 @@
 <div class="absolute top-0 flex flex-row w-full bg-happy-dark py-1 justify-center z-50">
 	<p class="text-happy-light text-base font-bold antialiased whitespace-nowrap">
-		BNB PRICE: $212.21 • CAKEBOT PRICE: $12.42 • STAKE APR: 51.21% • BOT VOLUME(24HR): $50,212.22 •
-		CURRENT BLOCK: 1246734
+		BNB PRICE: $212.21 • HAPPY TOKEN PRICE: $12.42 • STAKE APR: 51.21% • BOT VOLUME(24HR):
+		$50,212.22 • CURRENT BLOCK: 1246734
 	</p>
 </div>
